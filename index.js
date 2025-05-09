4b39d25673cf38926333b47af7511d7dfd6c8f9c
antnose <antnose28@gmail.com>
2025-05-09 17:13:58 +0600

    complete basic setup of mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -5,14 +6,43 @@ const port = process.env.PORT || 9000;
 const app = express();
 const morgan = require("morgan");
 
+// All Middleware here
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get('/',(req,res)=>{
-  res.send('Hello from SM TECH Server')
-})
+const uri =
+  "mongodb+srv://asdf:asdf@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(port,()=>{
-  console.log("SM Tech is Running on port : ",port)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from SM TECH Server");
+});
+
+app.listen(port, () => {
+  console.log("SM Tech is Running on port : ", port);
+});
