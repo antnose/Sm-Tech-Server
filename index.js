8fa2ac8abef9263956652c395ee357d9848416a1
Tanvir <zishantanvir05@gmail.com>
2025-05-10 09:58:42 +0600

    just regenerate pass

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 const uri =
-  "mongodb+srv://asdf:asdf@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
